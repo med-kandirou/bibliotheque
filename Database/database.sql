99d7b7984949a6f5e82f361917bce62987346a69
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-09-01 10:15:46 +0100

    third commit

---
+++ Database/database.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE biblio;
+USE biblio;
+
+CREATE TABLE livre (
+    isbn VARCHAR(13) PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    auteur VARCHAR(255),
+    quntite int
+);
+
+CREATE TABLE exemplaire (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    isbn VARCHAR(13),
+    statut ENUM('disponible', 'emprunté', 'perdu'),
+    FOREIGN KEY (isbn) REFERENCES livre(isbn)
+);
+
+CREATE TABLE emprunteur (
+    Num INT AUTO_INCREMENT PRIMARY KEY,
+    Nom VARCHAR(255),
+    Prenom VARCHAR(255),
+    Email VARCHAR(255),
+    Tele VARCHAR(20)
+);
+
+CREATE TABLE emprunt (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    emprunteur_id INT,
+    exemplaire_id INT,
+    date_emprunt DATE,
+    date_retour DATE,
+    FOREIGN KEY (emprunteur_id) REFERENCES emprunteur(Num),
+    FOREIGN KEY (exemplaire_id) REFERENCES exemplaire(id)
+);
+
