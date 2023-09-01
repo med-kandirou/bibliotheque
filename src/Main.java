edf66b43e91adece3cd5cf50e3f0ef8fd9bd7298
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-09-01 09:43:17 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
+
         }
     }
 }
