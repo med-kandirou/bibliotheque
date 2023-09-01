16a796fb411fb4681372058cd3fcd7ded60374f8
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-09-01 14:40:14 +0100

    update structure commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welome!");
+        System.out.print("ttttt");
     }
 }
