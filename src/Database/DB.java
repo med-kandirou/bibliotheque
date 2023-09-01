421ca123479dc69151d9d45ce42b70528744668b
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-09-01 15:06:14 +0100

    add database class

--- src/Database/DB.java
+++ src/Database/DB.java
@@ -1,5 +1,23 @@
 package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DB {
-
+    public static Connection connect(){
+        Connection con=null;
+        String jdbcUrl = "jdbc:mysql://localhost:3306/biblio";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(jdbcUrl, "root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return con;
+    }
 }
+
+
+
