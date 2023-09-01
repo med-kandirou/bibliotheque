312391ba2cd83e391b3c82d5e2643f051a3016f9
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-09-01 19:24:46 +0100

    singlton implementation

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,45 @@
+package Database;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+
+    // Private constructor to prevent direct instantiation
+    private DatabaseConnection() {
+    }
+
+    // Public method to get the single instance of DatabaseConnection
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    public static Connection connect() {
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String Url = prop.getProperty("DatabaseConnection.url");
+        String Username = prop.getProperty("DatabaseConnection.username");
+        String Password = prop.getProperty("DatabaseConnection.password");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(Url, Username, Password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import Database.DB;
+import Database.DatabaseConnection;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print(DB.connect());
+        DatabaseConnection obj = DatabaseConnection.getInstance();
+        System.out.print(obj.connect());
     }
 }
